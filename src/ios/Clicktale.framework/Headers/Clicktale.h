597439b9aab2cc6a23620b1304f28884296fd700
Omer Blumenkrunz <Omer.Blumenkrunz@clicktale.com>
2018-03-25 10:11:58 +0300

    Update Clicktale iOS SDK 2.1.18 Fixes for PII misplacements

--- src/ios/Clicktale.framework/Headers/Clicktale.h
+++ src/ios/Clicktale.framework/Headers/Clicktale.h
@@ -4,7 +4,7 @@
 //
 //  Created by Omer Blumenkrunz on 22/12/2016.
 //  Copyright (c) 2017 Clicktale, Inc. All rights reserved.
-//  Version : 2.1.17
+//  Version : 2.1.18
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
